a167f6e0f62cfceec454a4a40e6093439293a6a2
Waldo Lavaut <w86compositor@gmail.com>
2018-05-29 22:58:13 -0400

    Added the endpoint

--- wiki.js
+++ wiki.js
@@ -1,3 +1,10 @@
 $(document).ready( () => {
+
   console.log("ready!!!");
+
+
+  let endpoint = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='
+
+
+
 });
