ec557868ea8198b785afb5e52be97d3786f61784
Waldo Lavaut <w86compositor@gmail.com>
2018-06-02 19:32:11 -0400

    Added the random article's button

--- wiki.js
+++ wiki.js
@@ -1,11 +1,17 @@
 $(document).ready(() => {
-
 	console.log("ready!!!");
-
 
 	let endpoint = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='
 
-	function getRequest() {
+
+	$("#random").click(() => {
+		return window.open("https://en.wikipedia.org/wiki/Special:Random")
+	});
+		
+	
+
+
+	function thisWholeThing() {
 
 		$("#search").keypress((e) => {
 			let searchTerm = $("#search").val();
@@ -21,7 +27,7 @@ $(document).ready(() => {
 				// Using jQuery
 				$.ajax({
 					url: endpoint + searchTerm,
-					dataType: 'jsonp', //notice that we're using JSONP to avoid CORS issues...
+					dataType: 'jsonp', // we're using JSONP to avoid CORS issues...
 					type: 'GET',
 					headers: {
 						'Api-User-Agent': 'Mozilla/5.0'
@@ -34,21 +40,18 @@ $(document).ready(() => {
 						// console.log(articles);
 
 						for (let value of Object.values(articles)) {
-							$("#results").append(`<a href="#" onclick="window.open('https://en.wikipedia.org/wiki/${value.title}')"         class="list-group-item list-group-item-action flex-column align-items-start">\
+							$("#results").append(`<a href="#" onclick="window.open('https://en.wikipedia.org/wiki/${value.title}')" class="list-group-item list-group-item-action flex-column align-items-start">\
 							<div class="d-flex w-100 justify-content-between">\
 								<h5 class="mb-1">${value.title}</h5>\
 							</div>\
 							<p class="mb-1">${value.extract}</p>\
 						</a>`);
-							// console.log(value.title);
-							// console.log(value.extract);
 						}
-					} 
+					}
 				});
 			}
 
 		})
 	}
-
-	getRequest();
+	thisWholeThing();
 });
